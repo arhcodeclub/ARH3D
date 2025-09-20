dbd1b12064282a92f1682a7e73b48adbbda508ee
TetieWasTaken <tjvssr@gmail.com>
2025-09-20 17:07:48 +0200

    feat: graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -19,14 +24,28 @@ func main() {
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Render the root template.
 		if err := tmpl.ExecuteTemplate(w, "index.html", nil); err != nil {
-				http.Error(w, "template error", 500)
-				return
+			http.Error(w, "template error", 500)
+			return
 		}
 	})
 
 	addr := ":8080"
+	srv := &http.Server{Addr: addr, Handler: mux}
+
+	// Graceful shutdown.
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		<-c
+		log.Println("Shutting down gracefully...")
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+		srv.Shutdown(ctx)
+	}()
+
 	log.Printf("Server listening on %s", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
-		log.Fatal(err)
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("ListenAndServe(): %v", err)
 	}
+	log.Println("Server stopped")
 }
